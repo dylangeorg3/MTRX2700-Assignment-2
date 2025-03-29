54ecf97db9c3c44b063723b58381411992673e85
Dylan George <dylan.george209638@gmail.com>
2025-03-30 00:15:43 +1100

    Update io_module.h

--- Exercise 1 - Digital IO/Inc/io_module.h
+++ Exercise 1 - Digital IO/Inc/io_module.h
@@ -1,10 +1,10 @@
-#ifndef IO_MODULE_H
-#define IO_MODULE_H
+#ifndef IO_MODULE.H
+#define IO_MODULE.H
 
+// Include standard STM32 libraries
 #include <stdint.h>
+#endif
 
-void io_init(void (*callback)(void));
-void io_set_led(uint8_t state);
-uint8_t io_get_led(void);
-
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
