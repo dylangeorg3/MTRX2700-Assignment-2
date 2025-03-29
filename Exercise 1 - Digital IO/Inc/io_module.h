e014c924edf9cafdad6f2708e78e5cc57943c0e7
Dylan George <dylan.george209638@gmail.com>
2025-03-29 23:52:18 +1100

    Create io_module.h

---
+++ Exercise 1 - Digital IO/Inc/io_module.h
@@ -0,0 +1,12 @@
+// io_module.h
+
+#ifndef IO_MODULE_H
+#define IO_MODULE_H
+
+#include <stdint.h>
+
+void io_init(void (*callback)(void));
+void io_set_led(uint8_t state);
+uint8_t io_get_led(void);
+
+#endif
